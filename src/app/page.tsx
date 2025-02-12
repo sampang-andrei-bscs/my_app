31b052f439cbfcddc2ce2d7e7c4f96fe57840f88
sampang-andrei-bscs <andreisampang19@gmail.com>
2025-02-12 10:42:49 +0800

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
 import Link from "next/link";
+import { db } from "~/server/db";
+import {SignedOut, SignedIn} from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 
-export default function HomePage() {
+export const dynamic = "force-dynamic";
+
+
+async function Images(){
+  const user = await auth();
+  if (!user.userId) throw new Error("Unauthorized");
+
+  
+  const images = await db.query.images.findMany({
+    where: (model) => eq(model.userId,user.userId),
+    orderBy: (model, {desc}) => desc (model.id),
+  });
+  return(
+<div className="flex flex-wrap justify-items-center gap-4">
+        {images.map((image) => (
+          <div key={image.id} className= "w-48"> 
+          <img src= {image.url} />
+          <div>{image.name}</div>
+          </div>
+
+          
+        ))}
+
+      </div>
+  );
+}
+
+export default async function HomePage() {
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     Hello
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-2x1 text-center">Please Sign In Above</div>
+      </SignedOut>
+
+      <SignedIn>
+       <Images />
+      </SignedIn>
+      
     </main>
   );
 }
