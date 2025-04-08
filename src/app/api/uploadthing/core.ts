39df03d590b7c3531cbb1fc2fc7e69f46b6d7345
sampang-andrei-bscs <andreisampang19@gmail.com>
2025-04-08 22:49:11 +0800

    select user to upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
@@ -30,6 +30,11 @@ export const ourFileRouter = {
       // If you throw, the user will not be able to upload
       // eslint-disable-next-line @typescript-eslint/only-throw-error
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      //Select only who can upload using clerk middleware
+      const fullUserData = await (await clerkClient()).users.getUser(user.userId);
+
+      if (fullUserData?.privateMetadata?.["can-upload"] !== true) throw new UploadThingError("User Does Not Have Upload Permission");
 
       //rate limit user
       const { success } = await ratelimit.limit(user.userId);
